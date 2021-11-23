6e57c2c58a66245b8dbb4831400d17707fe14373
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2021-11-22 19:40:35 -0800

    Made additions to index.js to make the budget tracker

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-let weeklyExpenses = 0
-let monthlyExpenses = 0
-let annualExpenses = 0
+let weeklyExpenses = 0;
+let monthlyExpenses = 0;
+let annualExpenses = 0;
 
-// Your Code Here
+//Poll user for the expenses. 
+for (let i = 0; i < weeklyExpenseQuestions.length; i++){
+    weeklyExpenses += parseFloat(window.prompt(weeklyExpenseQuestions[i]));
+}    
+for (let i = 0; i < monthlyExpenseQuestions.length; i++){
+    weeklyExpenses += parseFloat(window.prompt(monthlyExpenseQuestions[i]));
+} 
+for (let i = 0; i < annualExpenseQuestions.length; i++){
+    weeklyExpenses += parseFloat(window.prompt(annualExpenseQuestions[i]));
+} 
